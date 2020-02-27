16b67e603e78576a662bb7bfba7fb5b79a0db2a9
gacosta95 <gabrielbbc_14@hotmail.com>
2020-02-27 05:02:04 -0300

    Exc04 Solução

--- Estrutura de decisão/Exc04.py
+++ Estrutura de decisão/Exc04.py
@@ -1,2 +1,9 @@
-#Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
-print("Teste")
+# Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+vogais = ["a", "e", "i", "o", "u", "y", "w"]
+letra = str(input("Letra: "))
+
+if letra in vogais:
+    print("Vogal!")
+
+else:
+    print("Consoante!")
